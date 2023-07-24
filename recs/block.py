4360f481e2b9f0c1ed7d187ea08450533df1c2f8
Tom Ritchford <tom@swirly.com>
2023-07-24 18:02:44 +0200

    Split off device.py and block.py

---
+++ recs/block.py
@@ -0,0 +1,46 @@
+from . import Array, DType
+import dataclasses as dc
+
+
+@dc.dataclass(frozen=True)
+class Block:
+    block: Array
+
+    def __len__(self) -> int:
+        return self.block.shape[0]
+
+    @cached_property
+    def amplitude(self) -> DType:
+        return self.block.max() - self.block.min()
+
+
+class Blocks:
+    blocks: list[Block] = dc.field(default_factory=list)
+    length: int = 0
+
+    def append(self, block):
+        self.blocks.append(block)
+        self.length += len(block)
+
+    def clear(self):
+        self.length = 0
+        self.blocks.clear()
+
+    def clip_to_length(self, sample_length: int):
+        removed = []
+        while self.length > sample_length:
+            block = self.blocks.pop(0)
+            removed.append(block)
+            self.length -= len(block)
+        return removed
+
+    def write(self, sf):
+        for b in self.blocks:
+            sf.write(b)
+        self.clear()
+
+    def __getitem__(self, i):
+        return self.blocks[i]
+
+    def __len__(self):
+        return len(self.blocks)
---
+++ recs/device.py
@@ -0,0 +1,39 @@
+from functools import cached_property
+import dataclasses as dc
+import sounddevice as sd
+import sys
+
+
+@dc.dataclasses
+class Device:
+    device: int | str
+    queue: Queue = dc.field(default_factory=Quene)
+    block_count: int = 0
+
+    @cached_property
+    def channels(self) -> int:
+        return self.info['max_input_channels')
+
+    @cached_property
+    def info(self):
+        return sd.query_devices(self.device, 'input')
+
+    @cached_property
+    def samplerate(self) -> int:
+        return int(self.info['default_samplerate'])
+
+    def callback(self, indata, frames, time, status):
+        if status:
+            print(status, file=sys.stderr)
+        self.queue.put(indata.copy())
+        self.block_count += 1
+
+    @cached_property
+    def input_stream(self):
+        return sd.InputStream(
+            callback=self.callback,
+            channels=self.channels,
+            device=self.device,
+            dtype=DType,
+            samplerate=self.samplerate,
+        )
