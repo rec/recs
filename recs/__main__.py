1c9926c5ac561aabc0df1cf2485c5e7a58389d8f
Tom Ritchford <tom@swirly.com>
2023-07-29 17:00:23 +0200

    Implement CLI

---
+++ recs/__main__.py
@@ -0,0 +1,39 @@
+from typer import Typer
+import sys
+
+ICON = '🎙'
+CLI_NAME = 'recs'
+
+app = Typer(
+    add_completion=False,
+    context_settings={'help_option_names': ['--help', '-h']},
+    help=f"""\
+{ICON} {CLI_NAME} {ICON}
+
+Usage: {CLI_NAME} [GLOBAL-FLAGS] [COMMAND] [COMMAND-FLAGS] [COMMAND-ARGS]
+""")
+
+command = app.command
+
+
+@command(help='Record')
+def rec():
+    pass
+
+
+@command(help='Check levels')
+def check():
+    pass
+
+
+def run():
+    try:
+        app(standalone_mode=False)
+    except click.ClickException as e:
+        return f'{e.__class__.__name__}: {e.message}'
+    except click.Abort:
+        return 'Aborted'
+
+
+if __name__ == '__main__':
+    sys.exit(run())
