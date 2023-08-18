3ee25fc48976872dcaf11979335f5675a6ab0f52
Tom Ritchford <tom@swirly.com>
2023-08-18 18:04:33 +0200

    Add more to checker

--- recs/checker.py
+++ recs/checker.py
@@ -1,21 +1,65 @@
-from . import audio_display, device, mux, threads
+import dataclasses as dc
+import time
+import typing as t
+from copy import deepcopy
+from functools import cached_property
+
 from rich.console import Console
 from rich.live import Live
-import time
+from rich.table import Table
+
+from . import audio_display, device, field, mux, threads
 
 FLOW_SLICE = mux.auto_slice(8) | {'Main': slice(8, 10)}
 DEVICE_SLICES = {'FLOW': FLOW_SLICE}
+CONSOLE = Console(color_system='truecolor')
+InputDevice = device.InputDevice
 
 
-def check():
+def check_old():
     devices = device.input_devices()
     g = audio_display.Global()
-    console = Console(color_system='truecolor')
+
     running = threads.IsRunning()
     running.start()
 
-    with mux.demux_context(devices.values(), g, running.stop, DEVICE_SLICES):
-        with Live(g.table(), refresh_per_second=4, console=console) as live:
+    with mux.demux_context(devices.values(), g.callback, running.stop, DEVICE_SLICES):
+        with Live(g.table(), refresh_per_second=4, console=CONSOLE) as live:
             while running.running:
                 time.sleep(0.01)
                 live.update(g.table())
+
+
+def check():
+    g = audio_display.Global()
+    Monitor(g.callback, g.table).run()
+
+
+@dc.dataclass
+class Monitor(threads.IsRunning):
+    callback: device.Callback
+    table: t.Callable[[], Table]
+
+    device_slices: dict[str, dict[str, slice]] = field(lambda: deepcopy(DEVICE_SLICES))
+    devices: device.InputDevices = field(device.input_devices)
+    refresh_per_second: float = 4
+    sleep_time: float = 0.01
+
+    @cached_property
+    def live(self):
+        table = self.table()
+        return Live(table, refresh_per_second=self.refresh_per_second, console=CONSOLE)
+
+    @cached_property
+    def context(self):
+        return mux.demux_context(
+            self.devices.values(), self.callback, self.stop, self.device_slices
+        )
+
+    def run(self):
+        self.start()
+        mux.demux_context(self.devices.values(), self, self.device_slices)
+        with self.live, self.context:
+            while self.running:
+                time.sleep(self.sleep_time)
+                self.live.update(self.listener.table())
