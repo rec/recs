144a2fb947657e65f9a148de2975c54de7f00180
Tom Ritchford <tom@swirly.com>
2023-09-23 12:55:10 +0200

    Split off cli/info.py

--- recs/cli/__init__.py
+++ recs/cli/__init__.py
@@ -1,5 +1,3 @@
-import typing as t
-
 import click
 import dtyper as ty
 
@@ -31,30 +29,6 @@ def check():
 
     top = audio_display.DevicesCallback()
     monitor.Monitor(top.callback, top.table).run()
-
-
-@command(help='Info')
-def info(
-    kind: t.Optional[str] = ty.Argument(None),
-):
-    import json
-
-    import sounddevice as sd
-
-    info = sd.query_devices(kind=kind)
-
-    def accept(k):
-        if 'channel' in k:
-            return k.replace('max_', '')
-        if 'sample' in k:
-            return 'sample_rate'
-
-    def to_entry(d):
-        return d['name'], {j: v for k, v in d.items() if (j := accept(k))}
-
-    info = dict(to_entry(d) for d in info)
-
-    print(json.dumps(info, indent=2))
 
 
 def run():
---
+++ recs/cli/info.py
@@ -0,0 +1,29 @@
+import typing as t
+
+import dtyper
+
+from . import command
+
+
+@command(help='Info')
+def info(
+    kind: t.Optional[str] = dtyper.Argument(None),
+):
+    import json
+
+    import sounddevice as sd
+
+    info = sd.query_devices(kind=kind)
+
+    def accept(k):
+        if 'channel' in k:
+            return k.replace('max_', '')
+        if 'sample' in k:
+            return 'sample_rate'
+
+    def to_entry(d):
+        return d['name'], {j: v for k, v in d.items() if (j := accept(k))}
+
+    info = dict(to_entry(d) for d in info)
+
+    print(json.dumps(info, indent=2))
