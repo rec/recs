c258b745c604fa4c97548262c0ea38bb1015d1c6
Tom Ritchford <tom@swirly.com>
2023-09-24 13:01:27 +0200

    Inline audio/info.py

--- recs/audio/recs.py
+++ recs/audio/recs.py
@@ -1,20 +1,41 @@
+import json
+import typing as t
+
 import dtyper
+import sounddevice as sd
 
 from recs.__main__ import recs
 from recs.ui import audio_display
 
-from . import info, monitor
+from . import monitor
+
+
+def run(d):
+    return Recs(**d)()
 
 
 @dtyper.dataclass(recs)
 class Recs:
     def __call__(self):
         if self.info:
-            info.info()
+            _info()
         else:
             top = audio_display.DevicesCallback()
             monitor.Monitor(top.callback, top.table).run()
 
 
-def run(d):
-    return Recs(**d)()
+def _info(kind: t.Optional[str] = None):
+    info = sd.query_devices(kind=kind)
+
+    def accept(k):
+        if 'channel' in k:
+            return k.replace('max_', '')
+        if 'sample' in k:
+            return 'sample_rate'
+
+    def to_entry(d):
+        return d['name'], {j: v for k, v in d.items() if (j := accept(k))}
+
+    info = dict(to_entry(d) for d in info)
+
+    print(json.dumps(info, indent=2))
