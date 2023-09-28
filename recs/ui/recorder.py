bca501882deffdfe6f4434c62114c5d1060f42f3
Tom Ritchford <tom@swirly.com>
2023-09-28 16:53:27 +0200

    Inline audio/callback.py into recorder.py

--- recs/ui/recorder.py
+++ recs/ui/recorder.py
@@ -1,29 +1,92 @@
 import dataclasses as dc
 import time
 import typing as t
+from abc import ABC, abstractmethod
+from functools import cached_property
+from threading import Lock
 
 from rich.table import Table
 
 from recs import cli, field
-from recs.audio import callback, mux
-from recs.ui.counter import Accumulator, Counter
-from recs.ui.table import TableFormatter
+from recs.audio.mux import AudioUpdate
+
+from .counter import Accumulator, Counter
+from .table import TableFormatter
+
+
+class HasRows(ABC):
+    @abstractmethod
+    def rows(self) -> t.Iterator[dict[str, t.Any]]:
+        pass
+
+    @abstractmethod
+    def callback(self, u: AudioUpdate) -> None:
+        pass
+
+
+class Maker(HasRows, ABC):
+    def __init__(self) -> None:
+        self._lock = Lock()
+        self.contents: t.Dict[str, HasRows] = {}
+
+    def __post_init__(self):
+        Maker.__init__(self)
+
+    @cached_property
+    def start_time(self):
+        return time.time()
+
+    @property
+    def elapsed_time(self):
+        return time.time() - self.start_time
+
+    key_name: str
+
+    @abstractmethod
+    def make(self, u: AudioUpdate) -> HasRows:
+        pass
+
+    def rows(self) -> t.Iterator[dict[str, t.Any]]:
+        for v in self.contents.values():
+            yield from v.rows()
+
+    def callback(self, u: AudioUpdate) -> None:
+        name = getattr(u, self.key_name)
+
+        with self._lock:
+            try:
+                has_rows = self.contents[name]
+            except KeyError:
+                self.contents[name] = has_rows = self.make(u)
+        has_rows.callback(u)
+
+
+class DeviceCallback(Maker):
+    """This class gets callbacks from blocks from a single device"""
+
+    key_name = 'channel_name'
+
+
+class DevicesCallback(Maker):
+    """This class gets callbacks from all blocks from all devices"""
+
+    key_name = 'device_name'
 
 
 @dc.dataclass
-class Recorder(callback.DevicesCallback):
+class Recorder(DevicesCallback):
     recording: cli.Recording
     block_count: Counter = field(Counter)
     start_time: float = field(time.time)
 
-    def make(self, u: mux.AudioUpdate) -> callback.HasRows:
+    def make(self, u: AudioUpdate) -> HasRows:
         return _DeviceCallback(u)
 
     @property
     def elapsed_time(self):
         return time.time() - self.start_time
 
-    def callback(self, u: mux.AudioUpdate) -> None:
+    def callback(self, u: AudioUpdate) -> None:
         self.block_count()
         super().callback(u)
 
@@ -39,16 +102,16 @@ class Recorder(callback.DevicesCallback):
 
 
 @dc.dataclass
-class _DeviceCallback(callback.DeviceCallback):
-    update: mux.AudioUpdate
+class _DeviceCallback(DeviceCallback):
+    update: AudioUpdate
     block_count: Counter = field(Counter)
     block_size: Accumulator = field(Accumulator)
     device_name: str = ''
 
-    def make(self, u: mux.AudioUpdate) -> callback.HasRows:
+    def make(self, u: AudioUpdate) -> HasRows:
         return _ChannelCallback(u)
 
-    def callback(self, u: mux.AudioUpdate):
+    def callback(self, u: AudioUpdate):
         self.block_count()
         assert u.block.channels <= 2, f'{len(u.block)=}, {u.block.channels=}'
         self.block_size(len(u.block))
@@ -65,8 +128,8 @@ class _DeviceCallback(callback.DeviceCallback):
 
 
 @dc.dataclass
-class _ChannelCallback(callback.HasRows):
-    update: mux.AudioUpdate
+class _ChannelCallback(HasRows):
+    update: AudioUpdate
     block_count: Counter = field(Counter)
     amplitude: Accumulator = field(Accumulator)
     rms: Accumulator = field(Accumulator)
@@ -74,7 +137,7 @@ class _ChannelCallback(callback.HasRows):
     channel_name: str = ''
     channel_count: int = 0
 
-    def callback(self, u: mux.AudioUpdate):
+    def callback(self, u: AudioUpdate):
         self.block_count()
         self.rms(u.block.rms)
         self.amplitude(u.block.amplitude)
