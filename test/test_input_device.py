3dfeda6123d4d017b06004524fb2032b89b2e098
Tom Ritchford <tom@swirly.com>
2023-10-03 12:23:08 +0200

    Add InputDevices class (see #3)

---
+++ test/test_input_device.py
@@ -0,0 +1,17 @@
+import pytest
+
+from recs.audio.device import InputDevice, InputDevices
+
+NAMES = 'One', 'ONE', 'Oner', 'Two', 'Three', 'FOUR'
+DEVICES = InputDevices(devices={n: InputDevice(info={'name': n}) for n in NAMES})
+
+
+@pytest.mark.parametrize('name', NAMES + ('tw', 'f'))
+def test_match(name):
+    DEVICES[name]
+
+
+@pytest.mark.parametrize('name', ('one', 'x', 't', 'T'))
+def test_fail(name):
+    with pytest.raises(KeyError):
+        DEVICES[name]
