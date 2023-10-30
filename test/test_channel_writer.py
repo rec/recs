2a4b60c2a1215fc3f571ee0f7caf32a39a9f2e1c
Tom Ritchford <tom@swirly.com>
2023-10-30 10:49:28 +0100

    Refactor test_channel_writer.py

--- test/test_channel_writer.py
+++ test/test_channel_writer.py
@@ -1,3 +1,4 @@
+import dataclasses as dc
 from pathlib import Path
 
 import numpy as np
@@ -12,37 +13,51 @@ from recs.audio.file_types import DTYPE
 from recs.audio.track import Track
 from recs.misc.times import Times
 
+SAMPLERATES = 44_100, 48_000
+TIMES = {'silence_before_start': 30, 'silence_after_end': 40, 'stop_after_silence': 50}
+
 II = [np.array((1, -1, 1, -1), dtype=DTYPE)]
 OO = [np.array((0, 0, 0, 0), dtype=DTYPE)]
 
-ARRAYS1 = (17 * OO) + (4 * II) + (40 * OO) + II + (51 * OO) + (19 * II)
-RESULT1 = [[28, 16, 12], [28, 4, 12], [28, 76]]
 
-ARRAYS2 = (4 * II) + (3 * OO) + II + (2000 * OO) + (3 * II)
-RESULT2 = [[0, 16, 12, 4, 12], [28, 12]]
-
-ARRAYS3 = 100 * II
-RESULT3 = [[0, 210], [0, 190]]
-
-SAMPLERATE = 48_000
-
-TESTS = ((ARRAYS1, RESULT1, 0), (ARRAYS2, RESULT2, 0), (ARRAYS3, RESULT3, 210))
-TIMES = {'silence_before_start': 30, 'silence_after_end': 40, 'stop_after_silence': 50}
+@dc.dataclass
+class Case:
+    arrays: np.ndarray
+    result: list[list[int]]
+    longest_file_time: int = 0
 
 
-@pytest.mark.parametrize('arrays, segments, longest_file_time', TESTS)
+TEST_CASES = (
+    Case(
+        arrays=(17 * OO) + (4 * II) + (40 * OO) + II + (51 * OO) + (19 * II),
+        result=[[28, 16, 12], [28, 4, 12], [28, 76]],
+    ),
+    Case(
+        arrays=(4 * II) + (3 * OO) + II + (2000 * OO) + (3 * II),
+        result=[[0, 16, 12, 4, 12], [28, 12]],
+    ),
+    Case(
+        arrays=100 * II,
+        result=[[0, 210], [0, 190]],
+        longest_file_time=210,
+    ),
+)
+
+
+@pytest.mark.parametrize('case', TEST_CASES)
 @tdir
-def test_channel_writer(arrays, segments, longest_file_time, mock_devices, monkeypatch):
+def test_channel_writer(case, mock_devices, monkeypatch):
     monkeypatch.setattr(RECS, 'format', 'wav')
     track = Track('Ext', '2')
-    times = Times[int](longest_file_time=longest_file_time, **TIMES)
+    times = Times[int](longest_file_time=case.longest_file_time, **TIMES)
 
     with ChannelWriter(times=times, track=track) as writer:
-        [writer.write(Block(a)) for a in arrays]
+        [writer.write(Block(a)) for a in case.arrays]
 
     contents, samplerates = zip(*(sf.read(f) for f in sorted(Path('.').iterdir())))
 
-    assert segments == [list(_on_and_off_segments(c)) for c in contents]
+    assert all(s in SAMPLERATES for s in samplerates)
+    assert case.result == [list(_on_and_off_segments(c)) for c in contents]
 
 
 def _on_and_off_segments(it):
