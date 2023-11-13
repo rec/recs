4c9e39e16a6f3f9b9d6c71f5029def1afbb8f147
Tom Ritchford <tom@swirly.com>
2023-11-13 18:23:26 +0100

    Handle non-existent files in FileList

--- recs/misc/file_list.py
+++ recs/misc/file_list.py
@@ -5,7 +5,8 @@ from pathlib import Path
 class FileList(list[Path]):
     """
     A list of paths with a total size. The size of the last file, only, in the list
-    might be updated after it is added.
+    might be updated after it is added.  Missing files are not reported and count
+    as zero bytes.
     """
 
     _count: int = 0
@@ -17,7 +18,14 @@ class FileList(list[Path]):
             return 0
 
         while self._count + 1 < len(self):
-            self._total_size += os.path.getsize(self[self._count])
+            self._total_size += _getsize(self[self._count])
             self._count += 1
 
-        return self._total_size + os.path.getsize(self[-1])
+        return self._total_size + _getsize(self[-1])
+
+
+def _getsize(p) -> int:
+    try:
+        return os.path.getsize(p)
+    except FileNotFoundError:
+        return 0
