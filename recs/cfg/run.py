5ace1dfd1b0d0f9a4902f95c131cca3548d62987
Tom Ritchford <tom@swirly.com>
2023-11-18 13:38:21 +0100

    Add sounddevice test code into recs --info

--- recs/cfg/run.py
+++ recs/cfg/run.py
@@ -1,6 +1,8 @@
 import json
+import multiprocessing as mp
+import subprocess as sp
+import time
 
-import sounddevice as sd
 import soundfile as sf
 
 from recs.base.types import Format, SdType
@@ -11,9 +13,7 @@ from . import Cfg
 
 def run(cfg: Cfg) -> None:
     if cfg.info:
-        info = sd.query_devices(kind=None)
-        info = [i for i in info if i['max_input_channels']]
-        print(json.dumps(info, indent=4))
+        _info()
 
     elif cfg.list_types:
         avail = sf.available_formats()
@@ -26,3 +26,41 @@ def run(cfg: Cfg) -> None:
 
     else:
         Recorder(cfg).run()
+
+
+def _info():
+    import sounddevice as sd
+
+    info = sd.query_devices(kind=None)
+    info = [i for i in info if i['max_input_channels']]
+    print(json.dumps(info, indent=4))
+
+
+if False:
+
+    def _info2():
+        while True:
+            _query()
+            time.sleep(1)
+
+    def _info3():
+        assert mp and sp
+
+        while True:
+            sp.run('python -m sounddevice'.split())
+            time.sleep(2)
+
+    def _query():
+        import sounddevice as sd
+
+        info = sd.query_devices(kind=None)
+        print(sorted(i['name'] for i in info if i['max_input_channels']))
+
+    def _info4():
+        assert mp and sp
+        mp.set_start_method('fork')
+        _query()
+
+        while True:
+            mp.Process(target=_query).start()
+            time.sleep(2)
