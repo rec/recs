9e91d9f04695f29c621ae9d10c45c2d0f165bc18
Tom Ritchford <tom@swirly.com>
2023-11-26 16:13:22 +0100

    Rename total_state to total

--- recs/ui/total_state.py
+++ recs/ui/total_state.py
@@ -11,7 +11,7 @@ class TotalState:
             return {i.name: state.ChannelState() for i in t}
 
         self.state = {k.name: device_state(v) for k, v in tracks.items()}
-        self.total_state = state.ChannelState()
+        self.total = state.ChannelState()
         self.start_time = times.time()
 
     @property
@@ -22,17 +22,17 @@ class TotalState:
         for device_name, device_state in state.items():
             for channel_name, channel_state in device_state.items():
                 self.state[device_name][channel_name] += channel_state
-                self.total_state += channel_state
+                self.total += channel_state
                 if '-' in channel_name:
                     # Hack to fix #96, stereo channels in total time
-                    self.total_state.recorded_time += channel_state.recorded_time
+                    self.total.recorded_time += channel_state.recorded_time
 
     def rows(self) -> t.Iterator[dict[str, t.Any]]:
         yield {
             'time': self.elapsed_time,
-            'recorded': self.total_state.recorded_time,
-            'file_size': self.total_state.file_size,
-            'file_count': self.total_state.file_count,
+            'recorded': self.total.recorded_time,
+            'file_size': self.total.file_size,
+            'file_count': self.total.file_count,
         }
 
         for device_name, device_state in self.state.items():
