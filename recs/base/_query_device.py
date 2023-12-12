4a5382beeccb7f867e269404b89f67d965a0fb46
Tom Ritchford <tom@swirly.com>
2023-12-12 12:45:05 +0100

    Document, add typing to _query_device.py

--- recs/base/_query_device.py
+++ recs/base/_query_device.py
@@ -1,9 +1,21 @@
-if __name__ == '__main__':
-    try:
-        import json
+"""
+Print the current devices as JSON without loading any other part of recs.
 
+Called repeatedly as a subprocess to detect devices going off- and online.
+"""
+
+import json
+import typing as t
+
+
+def query_devices() -> list[dict[str, t.Any]]:
+    try:
         import sounddevice
 
-        print(json.dumps(sounddevice.query_devices(), indent=4))
-    except BaseException:
-        print('[]')
+        return sounddevice.query_devices()
+    except Exception:
+        return []
+
+
+if __name__ == '__main__':
+    print(json.dumps(query_devices(), indent=4))
