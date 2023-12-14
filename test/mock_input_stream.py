e53c9f2fb2351425366eba8cd02fb56ad20feba8
Tom Ritchford <tom@swirly.com>
2023-12-14 13:56:05 +0100

    Modify test/mock_input_stream.py

--- test/mock_input_stream.py
+++ test/mock_input_stream.py
@@ -1,5 +1,4 @@
 import random
-from functools import cached_property
 from test.conftest import SLEEP_TIME
 
 import numpy as np
@@ -22,11 +21,18 @@ class InputStream(sd.InputStream):
         self.__count = 0
 
         seed = int.from_bytes(self.device.encode(), byteorder='big')
-        self.__rng = np.random.default_rng(seed)
         self.__random = random.Random(seed)
         self.__thread = HasThread(
             self.__callback, looping=True, name=f'Thread-{self.device}'
         )
+
+        shape = self.BLOCK_SIZE, self.channels
+        rng = np.random.default_rng(seed)
+        array = rng.uniform(-1 / 16, 1 / 16, size=shape)
+        assert self.dtype == 'float32'
+        assert array.dtype == np.double
+
+        self.__array = array.astype(self.dtype)
 
     def start(self) -> None:
         self.__thread.start()
@@ -36,16 +42,6 @@ class InputStream(sd.InputStream):
 
     def close(self, ignore_errors: bool = True) -> None:
         self.stop()
-
-    @cached_property
-    def __array(self) -> np.ndarray:
-        shape = self.BLOCK_SIZE, self.channels
-
-        array = self.__rng.uniform(-1 / 16, 1 / 16, size=shape)
-        assert self.dtype == 'float32'
-        assert array.dtype == np.double
-
-        return array.astype(self.dtype)
 
     def __callback(self) -> None:
         self.callback(self.__array, self.BLOCK_SIZE, 0, 0)
