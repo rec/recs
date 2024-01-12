76d52ad4eda92a53315e357dabfa524ededb136b
Tom Ritchford <tom@swirly.com>
2024-01-12 16:42:02 +0100

    Inline DeviceProcess into Recorder

--- recs/ui/recorder.py
+++ recs/ui/recorder.py
@@ -1,14 +1,14 @@
+import multiprocessing as mp
 import typing as t
 from multiprocessing import connection
 
-from threa import HasThread, IsThread, Runnables
+from threa import HasThread, Runnables, Wrapper
 
 from recs.base import RecsError
-from recs.cfg import Cfg, device
+from recs.cfg import Cfg, Track, device
 
 from . import live
-from .device_process import DeviceProcess
-from .device_recorder import POLL_TIMEOUT
+from .device_recorder import POLL_TIMEOUT, DeviceRecorder
 from .device_tracks import device_tracks
 from .full_state import FullState
 
@@ -23,53 +23,30 @@ class Recorder(Runnables):
         self.cfg = cfg
         self.live = live.Live(self.rows, cfg)
         self.state = FullState(tracks)
-        self.device_names = DeviceNames(cfg.sleep_time_device)
+        self.names = device.input_names()
 
-        processes = tuple(DeviceProcess(cfg, t) for d, t in tracks)
-        self.connections = {p.connection: p for p in processes}
+        def process(tracks: t.Sequence[Track]):
+            connection, child = mp.Pipe()
+            kwargs = {'cfg': cfg.cfg, 'connection': child, 'tracks': tracks}
+            process = mp.Process(target=DeviceRecorder, kwargs=kwargs)
+            return connection, process
+
+        c, p = zip(*(process(t) for _, t in tracks))
+        self.connections, self.processes = tuple(c), tuple(p)
+
         ui_time = 1 / self.cfg.ui_refresh_rate
         live_thread = HasThread(
             self.live.update, looping=True, name='LiveUpdate', pre_delay=ui_time
         )
 
-        self.runnables = self.device_names, *processes, live_thread, self.live
+        self.runnables = *(Wrapper(p) for p in self.processes), live_thread, self.live
 
     def run_recorder(self) -> None:
         with self:
-            while self.running:
-                self.receive()
-
-    def finish(self) -> None:
-        for r in reversed(self.runnables):
-            r.finish()
-        self.stop()
+            while self.running and all(p.is_alive() for p in self.processes):
+                for c in connection.wait(self.connections, timeout=POLL_TIMEOUT):
+                    conn = t.cast(connection.Connection, c)
+                    self.state.update(conn.recv())
 
     def rows(self) -> t.Iterator[dict[str, t.Any]]:
-        yield from self.state.rows(self.device_names.names)
-
-    def receive(self) -> None:
-        for conn in connection.wait(list(self.connections), timeout=POLL_TIMEOUT):
-            c = t.cast(connection.Connection, conn)
-            for device_name, msg in c.recv().items():
-                self.state.update({device_name: msg})
-
-        if (rt := self.cfg.total_run_time) and rt <= self.state.elapsed_time:
-            if True:
-                # TODO: it shouldn't make a difference, but this results in
-                # fewer tracebacks on control-c
-                self.stop()
-            else:
-                self.finish()
-
-
-class DeviceNames(IsThread):
-    daemon = True
-    looping = True
-
-    def __init__(self, pre_delay: float) -> None:
-        self.pre_delay = pre_delay
-        super().__init__()
-        self.callback()
-
-    def callback(self) -> None:
-        self.names = device.input_names()
+        yield from self.state.rows(self.names)
