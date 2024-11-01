02ad65576b47d1a7fae1e96ad5e10eab8d32388e
Chiemezuo <chiemezuoakujobi@gmail.com>
2024-11-02 00:22:31 +0100

    make cli test compatible for mac

--- test/cfg/test_cli.py
+++ test/cfg/test_cli.py
@@ -3,6 +3,6 @@ import subprocess as sp
 
 
 def test_info():
-    cmd = 'python -m recs --info'.split()
+    cmd = 'python -m recs --info'
     r = sp.run(cmd, text=True, check=True, stdout=sp.PIPE, shell=True).stdout
     json.loads(r)
