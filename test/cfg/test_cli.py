92dce1d01bb99ac0130f755b0497e9c07025241d
Chiemezuo <chiemezuoakujobi@gmail.com>
2024-11-02 09:10:27 +0100

    fix subprocess issue from windows store python installation

--- test/cfg/test_cli.py
+++ test/cfg/test_cli.py
@@ -4,5 +4,16 @@ import subprocess as sp
 
 def test_info():
     cmd = 'python -m recs --info'.split()
-    r = sp.run(cmd, text=True, check=True, stdout=sp.PIPE).stdout
-    json.loads(r)
+    try:
+        r = sp.run(cmd, text=True, check=True, stdout=sp.PIPE).stdout
+    except sp.CalledProcessError:
+        # On Windows, Python installation can also be via the Windows store
+        # When installed in this way, the python.exe acts as a "launcher" for the Python interpreter
+        # It may not behave like a standalone executable, which can lead to issues
+        # Especially when invoking subprocess commands.
+        # A safety check is to ask the subprocess to run in a shell
+        # This is an "except" case because not all Python installations are via Windows store
+        # And it'll be more overhead unless absolutely necessary
+        r = sp.run(cmd, text=True, check=True, stdout=sp.PIPE, shell=True).stdout
+    finally:
+        json.loads(r)
