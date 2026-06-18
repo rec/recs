3441d5e839701f39fe8f2c5193057a8a3cfa9b91
Tom Ritchford <tom@swirly.com>
2026-06-18 22:13:27 +0200

    Fix console script entry point

--- test/cfg/test_cli.py
+++ test/cfg/test_cli.py
@@ -1,5 +1,17 @@
+import importlib
 import json
 import subprocess as sp
+
+import tomli
+
+
+def test_console_script_entry_point() -> None:
+    project = tomli.loads(open('pyproject.toml').read())['project']
+    module_name, function_name = project['scripts']['recs'].split(':')
+
+    module = importlib.import_module(module_name)
+
+    assert callable(getattr(module, function_name))
 
 
 def test_info():
