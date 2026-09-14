8c27a0f1a352b2d877c29b496497f10555b981e7
Tom Ritchford <tom@swirly.com>
2026-09-14 11:45:26 +0200

    Update Ufor dependency

--- test/edit/test_documented_arrangement.py
+++ test/edit/test_documented_arrangement.py
@@ -15,4 +15,4 @@ def test_documented_arrangement_separates_ports_from_destinations() -> None:
     assert document.outputs[0].name == document.destinations[0].output
     assert 'path' not in document.outputs[0].model_dump()
     assert parse_edit(score_toml(document)) == document
-    assert ArrangementScore.model_json_schema()['properties']['body']
+    assert ArrangementScore.model_json_schema()['$defs']
